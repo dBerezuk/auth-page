4ff99fd67a8f5b83745f61e7bc2430aa45a0dab8
Denis <d_berezuk@icloud.com>
2024-07-10 18:07:40 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const authTypeBoxButtons = document.querySelector('.auth__top');
+const authTypeButtons = document.querySelectorAll('.auth__top-button');
+const authFieldGroups = document.querySelectorAll('.auth__field-group');
+
+authTypeBoxButtons.addEventListener('click', onAuthTypeButtons);
+
+function onAuthTypeButtons(e) {
+  const button = e.target.closest('.auth__top-button');
+  if (button) {
+    authFieldGroups.forEach(group => {
+      if (button.dataset.type === group.dataset.type) {
+        authFieldGroupsHides(authTypeButtons);
+        button.classList.add('is-active');
+
+        authFieldGroupsHides(authFieldGroups, true);
+
+        group.disabled = false;
+        return group.classList.add('is-active');
+      }
+    });
+  }
+}
+
+function authFieldGroupsHides(collection, disabled) {
+  collection.forEach(elem => {
+    if (disabled) elem.disabled = true;
+
+    elem.classList.remove('is-active');
+  });
+}
+
+IMask(document.querySelector('#tel'), {
+  mask: '+{38}(000)000-00-00',
+});
